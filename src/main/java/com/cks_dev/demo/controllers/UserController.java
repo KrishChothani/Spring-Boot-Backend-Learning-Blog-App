3bbf0730ae73aea7f19d04ef30ce6c0e1fa57cf7
Krish Chothani <143370415+KrishChothani@users.noreply.github.com>
2024-11-05 02:08:05 +0530

    USer Controller Completed Lect-12

--- src/main/java/com/cks_dev/demo/controllers/UserController.java
+++ src/main/java/com/cks_dev/demo/controllers/UserController.java
@@ -1,21 +1,22 @@
 package com.cks_dev.demo.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.cks_dev.demo.services.UserService;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.cks_dev.demo.payloads.ApiResponse;
 import com.cks_dev.demo.payloads.UserDto;
-
+import com.cks_dev.demo.services.UserService;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -24,12 +25,36 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    // Post-create User
+    // POST - Create User
     @PostMapping("/")
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
-        UserDto createdUSerDto = this.userService.createUser(userDto);
-        return new ResponseEntity<>(createdUSerDto, HttpStatus.CREATED);
+        UserDto createdUserDto = this.userService.createUser(userDto);
+        return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
     }
-    // PUT -update user
-    // Delete -delete user
+
+    // PUT - Update User
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Integer userId) {
+        UserDto updatedUser = this.userService.updateUser(userDto, userId);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    // DELETE - Delete User
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer userId) {
+        this.userService.deleteUser(userId);
+        return new ResponseEntity<>(new ApiResponse("User Deleted Successfully!", true), HttpStatus.OK);
+    }
+
+    // GET - All Users
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        return ResponseEntity.ok(this.userService.getAllUSers());
+    }
+
+    // GET - User By ID
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Integer userId) {
+        return ResponseEntity.ok(this.userService.getUserById(userId));
+    }
 }
